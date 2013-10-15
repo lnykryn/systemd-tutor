0cf5b1bec566104be77a6ac3d80dc55de56e30a3
Václav Pavlín <vpavlin@redhat.com>
2013-10-15 10:16:08 +0200

    rewrite screamer for journal API

--- src/screamer.c
+++ src/screamer.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <systemd/sd-journal.h>
 
 #define L_ALERT  "<1>"
 #define L_ERROR  "<3>"
@@ -10,10 +11,15 @@
 int main(int argc, char *argv[]) {
         pid_t pid;
 
-        fprintf(stderr, L_DEBUG "Hi, I am a Screamer and I will make some mess in your Journal log.\n");
-        fprintf(stderr, L_NOTICE "Let me note that you can see various message priorities in Journal and you can filter them.\n");
-        fprintf(stderr, L_ERROR "While previous messages are mostly informational, this one reports some error that happend in your system.\n");
-        fprintf(stderr, L_ALERT "Error and Alert get same color, but the latter is much more severe.\n");
+        sd_journal_print(LOG_DEBUG, "Hi, I am a Screamer and I will make some mess in your Journal log.");
+        sd_journal_print(LOG_NOTICE, "Let me note that you can see various message priorities in Journal and you can filter them.");
+        sd_journal_print(LOG_ERR, "While previous messages are mostly informational, this one reports some error that happend in your system.");
+        sd_journal_print(LOG_ALERT, "Error and Alert get same color, but the latter is much more severe.");
+
+        sd_journal_send("MESSAGE=You may find some interesting details about logged messages in stored fields. Try to list all fields of messages from process %lu.", (unsigned long) getpid(),
+                        "PRIORITY=%i", LOG_NOTICE,
+                        "HOME=%s", getenv("HOME"),
+                        NULL);
 
 //          pid = fork();
 
