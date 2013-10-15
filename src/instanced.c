fc1819045f83cd1b678c96a4089047a922383b94
Michal Sekletar <msekleta@redhat.com>
2013-10-15 15:08:47 +0200

    daemons: add instanced
    instanced is simple xinetd-like daemon. systemd forks off new daemon instance
    for each connection.

---
+++ src/instanced.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <readline/readline.h>
+
+enum greeting_type {
+	STAR_TREK = 0,
+	STAR_WARS,
+	SPANISH,
+	ENGLISH,
+	INDIAN,
+	_MAX_GREETING,
+	_GREETING_INVALID
+};
+
+static const char *greetings[_MAX_GREETING] = {
+	[STAR_TREK]  = "Live long and prosper",
+	[STAR_WARS]  = "May the Force with you",
+	[ENGLISH]    = "Hello there",
+	[SPANISH]    = "Hola",
+	[INDIAN]     = "Namaste"
+};
+
+int main() {
+	char *name = NULL;
+
+	srand(time(NULL));
+
+	printf("What is your name?\n");
+
+	name = readline(NULL);
+	if (!name)
+		return EXIT_FAILURE;
+
+        printf("%s %s!\n", greetings[rand() % _MAX_GREETING], name);
+
+	return 0;
+}
